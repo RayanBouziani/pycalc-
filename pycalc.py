b2c55771da40384918361ab93b846ff81c8bfdf9
Rayan Bouziani <22500549@me.uvsq.fr>
2026-02-26 12:01:09 +0100

    Version Multiplication sans *

--- pycalc.py
+++ pycalc.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def divisions(a,b):
     resul = a/b 
     return resul
@@ -13,12 +12,25 @@ def soustraction(a,b):
     resul = a - b
     return resul
 
-def multiplication(a,b):
-    resul = a*b
-    return resul
+def multiplication(a, b):
+    if b < 0:
+        signe = -1  
+    else:
+        signe = 1
+    b = abs(b)
+
+    partie_entiere = int(b)
+    partie_decimale = b - partie_entiere
+
+    resultat = 0
+    for _ in range(partie_entiere):
+        resultat += a
+
+    resultat += a * partie_decimale
+
+    return signe * resultat
 
 op1 = float(sys.argv[1])
 operrande = sys.argv[2]
 op2 = float(sys.argv[3])
-
 
