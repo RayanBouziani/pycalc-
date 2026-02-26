d7dfe1dae3a174e7059d4287be626f3303c71cc7
Rayan Bouziani <22500549@me.uvsq.fr>
2026-02-26 12:06:43 +0100

    Version FINALE MERGE

--- pycalc.py
+++ pycalc.py
@@ -13,8 +13,22 @@ def soustraction(a,b):
     return resul
 
 def multiplication(a, b):
-    resul = a *b
-    return resul
+    if b < 0:
+        signe = -1  
+    else:
+        signe = 1
+    b = abs(b)
+
+    partie_entiere = int(b)
+    partie_decimale = b - partie_entiere
+
+    resultat = 0
+    for _ in range(partie_entiere):
+        resultat += a
+
+    resultat += a * partie_decimale
+
+    return signe * resultat
 
 op1 = float(sys.argv[1])
 operrande = sys.argv[2]
